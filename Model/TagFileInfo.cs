57e822ce14024b0a5cd2817e8bc53ea5be73f047
Saul Scudder <Saul.Scudder@gmail.com>
2024-12-24 12:34:13 -0700

    Update TagFileInfo.cs
    An IsSelected schema chane in the database apeared after an update.

--- Model/TagFileInfo.cs
+++ Model/TagFileInfo.cs
@@ -15,6 +15,9 @@ namespace VidFileTag.Model
         [Key]
         public int Id { get; set; }
 
+         [Required]
+        public bool IsSelected { get; set; }
+
         [Required]
         public string FileName { get; set; } = string.Empty;
 
